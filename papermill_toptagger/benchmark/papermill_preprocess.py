08fe904c21c531e8400ecd6483e9e6bdc4975d81
anrunw <67667675+anrunw@users.noreply.github.com>
2021-06-08 04:22:35 -0700

    Delete papermill_toptagger directory

--- papermill_toptagger/benchmark/papermill_preprocess.py
+++
@@ -1,5 +0,0 @@
-import papermill as pm
-pm.execute_notebook('Preprocess.ipynb', 'output_preprocess_notebook.ipynb', parameters = dict(input_jets_file = 'data/test_jets.pkl',
-card_file = 'data/preprocess/jet_image_trim_pt800-900_card.dat',
-out_dir = 'data/results',
-transformer_file = 'data/preprocess/transformer.pkl'))

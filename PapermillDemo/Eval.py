9051ca57e049bc1ecc2c72c03f9d74af8c73d87f
anrunw <67667675+anrunw@users.noreply.github.com>
2021-06-08 04:22:23 -0700

    Delete PapermillDemo directory

--- PapermillDemo/Eval.py
+++
@@ -1,4 +0,0 @@
-import papermill as pm
-pm.execute_notebook('ROB notebook.ipynb', 'output notebook.ipynb', parameters = dict(model = 'model.h5',
-datafile = 'robfile.pkl',
-labels = ['j_g', 'j_q', 'j_w', 'j_z', 'j_t']))
